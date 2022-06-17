f73b130b31bb7a0b92b273dbe791948219720b6c
Miten Patel <patelmit3n@gmail.com>
2022-06-17 23:49:59 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Black Jack game
+
+Learning python by making small game and by applying some basic logic
---
+++ deck.py
@@ -0,0 +1,29 @@
+import random
+from card import Card
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+         'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+
+
+class Deck:
+
+    def __init__(self):
+        self.deck = []  # start with an empty list
+        for suit in suits:
+            for rank in ranks:
+                # build Card objects and add them to the list
+                self.deck.append(Card(suit, rank))
+
+    def __str__(self):
+        deck_comp = ''  # start with an empty string
+        for card in self.deck:
+            deck_comp += '\n '+card.__str__()  # add each Card object's print string
+        return 'The deck has:' + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
